a706d3eccd2c8230a2a453eb21ac8d36fcd56224
Omkar_Waje <124696368+omkarwaje2504@users.noreply.github.com>
2025-07-17 17:27:20 +0530

    add bio-data api

--- src/app/api/bio-data/route.js
+++ src/app/api/bio-data/route.js
@@ -1,1 +1,57 @@
-route
+
+
+/* ───────────────────── Shared CORS ───────────────────── */
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type",
+};
+
+/* ───────────────────── OPTIONS ───────────────────── */
+export async function OPTIONS() {
+  return new Response(null, { status: 204, headers: corsHeaders });
+}
+
+export async fucntion POST(request){
+  
+const body=await request.json();
+
+if(!body.employee_hash || !body.hash){
+ return new Response(
+      JSON.stringify({ success: false, message: "Give the data first" }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json", ...corsHeaders },
+      }
+    );
+}
+
+
+  const fetchData = await fetch(
+          `https://pixpro.app/api/employee/${body.employee_hash}/contact/${body.hash}`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+            },
+            body: JSON.stringify({
+              id: hash,
+            }),
+          },
+        );
+        const result = await response.json();
+
+  
+  
+
+   return new Response(
+      JSON.stringify({ success: true, message:result }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json", ...corsHeaders },
+      }
+    );
+
+
+}
